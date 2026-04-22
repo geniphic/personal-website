7b2c61e6edcd6980b5c949d4afad30a0217cc7cc
geni <lahaitaylorgenevieve@gmail.com>
2026-04-22 18:22:50 +0100

    Add order insertion logic in order.php

--- php/order.php
+++ php/order.php
@@ -1,1 +1,12 @@
+<?php
+$order_type = $_POST['order_type'];
+$order_status = "Pending"; // default when order is created
+
+$sql = "INSERT INTO orders (customer_id, order_type, order_status)
+        VALUES (?, ?, ?)";
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("iss", $customer_id, $order_type, $order_status);
+$stmt->execute();
+
 
